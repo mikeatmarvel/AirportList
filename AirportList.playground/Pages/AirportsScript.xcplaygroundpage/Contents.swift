f828a5e09a65801802e1a728b0113e205771e688
Devin Allen <tomas0512@gmail.com>
2022-05-20 10:49:27 -0400

    Remove comments and removed force unwraps safely

--- AirportList.playground/Pages/AirportsScript.xcplaygroundpage/Contents.swift
+++ AirportList.playground/Pages/AirportsScript.xcplaygroundpage/Contents.swift
@@ -10,21 +10,15 @@ let llong : Double = 21
 
 let airports = Airports.loadJSONFromFile(file: "airports", withExtension: "json")
 
-// Let's unrwap the object and make sure isn't nil
-//if let airports = airports {
-   //  Loop with high order functions (map, filter, reduce)
- //  airports.map { airports in
-  //      print(airports.csap, airports.lat, airports.long)
-//    }
-//}
 
-let terminal = airports?.first
-let location = terminal?.csap
-let termLat = Double(terminal!.lat)
-let termLong = Double(terminal!.long)
+guard let terminal = airports?.first,
+      let termLat = Double(terminal.lat),
+      let termLong = Double(terminal.long) else {
+    fatalError("Error with airport data")
+}
 
-terminal?.lat
-let appleParkWayCoordinates = CLLocationCoordinate2DMake(termLat!, termLong!)
+let location = terminal.csap
+let appleParkWayCoordinates = CLLocationCoordinate2DMake(termLat, termLong)
 
 // Now let's create a MKMapView
 let mapView = MKMapView(frame: CGRect(x:0, y:0, width:800, height:800))
@@ -42,8 +36,8 @@ mapView.setRegion(mapRegion, animated: true)
 // Create a map annotation
 let annotation = MKPointAnnotation()
 annotation.coordinate = appleParkWayCoordinates
-//annotation.title = "Apple Inc."
-//annotation.subtitle = "One Apple Park Way, Cupertino, California."
+annotation.title = location
+annotation.subtitle = "One Apple Park Way, Cupertino, California."
 
 mapView.addAnnotation(annotation)
 
